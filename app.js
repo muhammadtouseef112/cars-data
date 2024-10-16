059695737c68cd0a99849400bbf40f4f4bb8e196
@Muhammad Touseef <immuhammadtouseef@gmail.com>
2024-10-17 00:20:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// var obj = {
+//     brandname : "hundhai",
+//     carname : "civic",
+//     soldcar : "1000",
+//     profit : "20%"
+// }
+// console.log(obj);
+
+var carsdata = [
+    {
+        brandname : "hundhai",
+        carname : "civic",
+        soldcar : "967",
+        profit :"35%"
+    },
+    {
+        brandname : "toyata",
+        carname : "yaris",
+        soldcar : "467",
+        profit :"20%"
+    },
+    {
+        brandname : "audi",
+        carname : "audi a6",
+        soldcar : "200",
+        profit :"50%"
+    }
+  
+];
+console.log(carsdata[2].brandname) ;{
+    for (var i =0; i <= carsdata.length;i++){
+        console.log(carsdata[i])
+    }
+}
+
